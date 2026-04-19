b560692e5228757869eb5631da2e1f02dccccc79
Maria Correa <correahml@gmail.com>
2026-04-19 14:45:57 -0300

    suma de funciones: mostrar ganador

--- functions.js
+++ functions.js
@@ -1,14 +1,14 @@
 let scoreRojo = 0;
 let scoreAzul = 0;
-let setRojo = 0;
+let setRojo = 0; // GANADOR el que llegue a sets == 2
 let setAzul = 0;
 let winnerScore = 25;
 
 
 //funciones
-function addScore(equipo){
 
-    if (winnerTeam) return;
+function addScore(equipo){  //suma puntos. Agregado: hasta que algun equipo gane
+
     if (equipo === 'rojo') {
         scoreRojo++;
         document.getElementById("scoreRojo").textContent = scoreRojo;
@@ -16,34 +16,72 @@ function addScore(equipo){
         scoreAzul++;
         document.getElementById("scoreAzul").textContent = scoreAzul;
     }
+    setWin();
 }
 
-function subtractScore(equipo){
+function subtractScore(equipo){ //resta puntos
 
-    if (winnerTeam) return; //si el equipo gano no se puede seguir ocupando la funcion 
+    if (winnerTeam()) return; //verifica si algun equipo gano. alpedo creo
     if (equipo === 'rojo' && scoreRojo > 0){
         scoreRojo--;
         document.getElementById("scoreRojo").textContent = scoreRojo
     } else if (equipo === 'azul' && scoreAzul > 0){
         scoreAzul--;
         document.getElementById("scoreAzul").textContent = scoreAzul;
-        setWin(scoreAzul)
     }
-
 }
- // FUNCION findGanador: Al sumar un punto, se verifica si ese equipo ganó el set (≥25 puntos Y diferencia ≥2)
+
+ // FUNCION setWin dentro de add: Al sumar un punto, se verifica si ese equipo ganó el set (≥25 puntos Y diferencia ≥2)
 //Si ganó el set, se incrementa su contador de sets y se reinician los puntos del set
-//Si algún equipo llega a 3 sets, se muestra el ganador y se bloquean los botones
+//agregar si puedo: El equipo que llegue PRIMERO a 2 sets, se muestra el ganador y se bloquean los botones
 
 function setWin(){
-    if (scoreRojo === 25 && (scoreRojo - scoreAzul) >= 2) {
+    let ganador = null; //null cambia cuando alguno gane 1 set
+
+    if (scoreRojo >= 25 && scoreRojo - scoreAzul >= 2) {
+        ganador = "rojo"
+    } else if (scoreAzul >= 25 && scoreAzul - scoreRojo >= 2){
+        ganador = "azul"
+    }
+    // aca se suma ya 1 puntito de set para el equipo que corresponde
+     if (ganador === 'rojo') {
         setRojo++;
-    } else if (scoreAzul ===25 && scoreAzul - scoreRojo >= 2){
+        document.getElementById("setRojo").textContent = setRojo;
+    } else if (ganador === 'azul'){
         setAzul++;
+        document.getElementById("setAzul").textContent = setAzul;
+    }   
+    // si ganador ya no es null(porque null es falso) se verfifica si ya hay un ganador
+    if (ganador) {
+        if (winnerTeam()) return;
+        //si nadie gano el partido, se resetean los puntos
+        resetScore(); 
     }
 }
 
-function 
+//verifica que equipo gano y pasa como parametro ese equipo para mostrarlo
+function winnerTeam(){
+    if (setRojo === 2) {
+        showWinner('rojo');
+        return true;
+    } else if(setAzul === 2) {
+        showWinner('azul');
+        return true;
+    }
+    return false
+}
+
+//mostraria el ganador y bloquea el boton
+//FALTA: bloquear boton
+function showWinner(equipo) {
+
+    const msg = document.querySelector('.ganador');
+    msg.textContent = 'Gano el equipo ${equipo}!';
+    msg.style.color = equipo === 'rojo' ? '#e05c5c' : '#5c8ee0';
+
+    // Deshabilitar todos los botones
+    
+}
 
 function resetScore() {
 scoreAzul = 0;
