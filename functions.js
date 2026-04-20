f05fd2bfa8918073ec50a409f1effda05e76cdb8
Maria Correa <correahml@gmail.com>
2026-04-20 19:42:52 -0300

    anda casi todo menos deshabilitar botones

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ let scoreAzul = 0;
 let setRojo = 0; // GANADOR el que llegue a sets == 2
 let setAzul = 0;
 let winnerScore = 25;
+let empatados = false;
 
 
 //funciones
@@ -36,14 +37,27 @@ function subtractScore(equipo){ //resta puntos
 //agregar si puedo: El equipo que llegue PRIMERO a 2 sets, se muestra el ganador y se bloquean los botones
 
 function setWin(){
-    let ganador = null; //null cambia cuando alguno gane 1 set
+    let ganador = null; // null cambia cuando alguno gane 1 set
+    
+    let limite = 0
+    if (empatados) {
+        limite = 15;
+     }else if (!empatados) {
+        limite = 25;
+     } // si empatados es true el limite es 15 sino es 25
 
-    if (scoreRojo >= 25 && scoreRojo - scoreAzul >= 2) {
-        ganador = "rojo"
-    } else if (scoreAzul >= 25 && scoreAzul - scoreRojo >= 2){
-        ganador = "azul"
+    if (scoreRojo >= limite && scoreRojo - scoreAzul >= 2) {
+        ganador = "rojo";
+    } else if (scoreAzul >= limite && scoreAzul - scoreRojo >= 2) {
+        ganador = "azul";
+    
+    //} else if (scoreRojo >= 25 && scoreRojo - scoreAzul >= 2) {
+     //   ganador = "rojo"
+    //} else if (scoreAzul >= 25 && scoreAzul - scoreRojo >= 2){
+    //    ganador = "azul"
     }
-    // aca se suma ya 1 puntito de set para el equipo que corresponde
+
+    // Se suma 1 puntito de set para el equipo que corresponde
      if (ganador === 'rojo') {
         setRojo++;
         document.getElementById("setRojo").textContent = setRojo;
@@ -51,7 +65,8 @@ function setWin(){
         setAzul++;
         document.getElementById("setAzul").textContent = setAzul;
     }   
-    // si ganador ya no es null(porque null es falso) se verfifica si ya hay un ganador
+
+    // Si ganador ya no es null(false pasa a true) se verfifica si ya hay un ganador
     if (ganador) {
         if (winnerTeam()) return;
         //si nadie gano el partido, se resetean los puntos
@@ -67,19 +82,26 @@ function winnerTeam(){
     } else if(setAzul === 2) {
         showWinner('azul');
         return true;
+    } else if (setRojo === 1 && setAzul === 1) {
+        empatados = true
+        resetScore();
+        return false
     }
-    return false
+    return false;
 }
 
 //mostraria el ganador y bloquea el boton
-//FALTA: bloquear boton
 function showWinner(equipo) {
 
-    const msg = document.querySelector('.ganador');
-    msg.textContent = 'Gano el equipo ${equipo}!';
-    msg.style.color = equipo === 'rojo' ? '#e05c5c' : '#5c8ee0';
+    const msg = document.getElementById("ganador");
+    msg.textContent = ` Felicitaciones equipo ${equipo}! ⭐`;
+    // condicion en una linea: si el parametro pasado(equipo) es igual al valor comparado 
+    // ejecuta el primer valor(letra color rojo) sino ejecuta lo segundo(letra color azul)
+    msg.style.color = equipo === 'rojo' ? '#e05c5c' : '#5c8ee0'; 
 
-    // Deshabilitar todos los botones
+    // FALTA Deshabilitar todos los botones
+    const btn = document.getElementById("buttons");
+    btn.disabled = true;
     
 }
 
